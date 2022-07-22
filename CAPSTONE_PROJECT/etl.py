c6bc99e44fc024b905decf67cf1bc9d3abe25eda
rusty172 <101382920+rusty172@users.noreply.github.com>
2022-07-22 13:10:44 +1000

    Create etl.py

---
+++ CAPSTONE_PROJECT/etl.py
@@ -0,0 +1,45 @@
+import configparser
+import psycopg2
+from sql_queries import copy_table_queries, insert_table_queries
+
+"""
+    - Load staging immi & state table with immigration and us citiies demographics CSV files.
+    - Copy commands passed in the for loop for execution
+"""
+
+def load_staging_tables(cur, conn):
+    for query in copy_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+"""
+    - Process insert function on all the analytics table, insert queries are passed individually in the for loop
+"""
+
+def insert_tables(cur, conn):
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+"""
+    - Read the config file for connecting to the DB
+    - Call the load staging table function
+    - Call the insert query function
+    - Close the connection
+"""      
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+    
+    load_staging_tables(cur, conn)
+    insert_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
